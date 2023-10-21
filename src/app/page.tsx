7d0887f5d08fe3c58becd51ce036c42217f74830
firdaus <firdausthedev@gmail.com>
2023-10-21 13:59:17 +0800

    add spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import AppContext from "./components/context/app/AppContext";
 import { AppActionType } from "./components/context/app/AppType";
 import { searchByKey } from "./components/context/app/AppActions";
 import { WordData, WordDataError } from "./components/context/app/AppType";
+import Spinner from "./components/UI/Spinner";
 
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
@@ -15,12 +16,14 @@ export default function Home() {
 
   const onSearchHandler = async (key: string) => {
     if (key) {
+      dispatch({ type: AppActionType.SET_LOADING, payload: true });
       try {
         const data: WordDataError | WordData[] = await searchByKey(key);
         dispatch({ type: AppActionType.SET_RESPONSE, payload: data });
       } catch (error) {
         console.log(error);
       }
+      dispatch({ type: AppActionType.SET_LOADING, payload: false });
     }
   };
 
@@ -28,7 +31,13 @@ export default function Home() {
     <main className={`${state.fonts}`}>
       <SearchBar onChange={onChangeHandler} onSearch={onSearchHandler} />
 
-      {state.response && "message" in state.response && (
+      {state.loading && (
+        <div className="flex justify-center items-center mt-10">
+          <Spinner />
+        </div>
+      )}
+
+      {!state.loading && state.response && "message" in state.response && (
         <div className="mt-32 text-center gap-5 flex flex-col">
           <p className="text-6xl">😕</p>
           <h1 className="font-bold text-xl capitalize">
